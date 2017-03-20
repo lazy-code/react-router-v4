4c45a9ca6af7463669fd1455dcd5b9c465cfa20c
Lazy Code <fbenalex@gmail.com>
2017-03-20 12:15:58 +0200

    Query parameters

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Link
 } from 'react-router-dom';
+
+import './App.css';
+
+const Links = () => (
+  <nav>
+    <Link to='/?id=123'>Inline</Link>
+    <Link to={{pathname:'', search:'id=789'}}>Object</Link>
+  </nav>
+);
 
 const App = (props) => (
   <Router>
     <div>
+      <Links />
       <Route
-        path="/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)"
-        render={({match}) => (
-          <h1>
-            param a: {match.params.a}<br />
-            param b: {match.params.b}
-          </h1>
+        path="/" render={({match, location}) => (
+          <div>
+            <p>root</p>
+            <p>{JSON.stringify(match, '', 4)}</p>
+            <p>{JSON.stringify(location, '', 4)}</p>
+            <p>{new URLSearchParams(location.search).get('id')}</p>
+          </div>
         )} />
     </div>
   </Router>
